432b70ca44fba9d5788d1a72e2454cb5660a7c1d
WatskeBart <bartistic@gmail.com>
2021-11-29 14:46:33 +0100

    Status running voorbeeld

--- member-voorbeeld.ps1
+++ member-voorbeeld.ps1
@@ -2,3 +2,5 @@ $mijnservices = Get-Service #Stop alle services in een variabele
 $mijnservices[0] #Laat de eerste services in de variabele lijst zien
 $mijnservices[0] | get-member #Laat alle beschikbare members zien van de eerste service
 $mijnservices[0].DisplayName #Laat de DisplayName zien van de eerste service, waarbij DisplayName een Property (member) is van de service
+
+$mijnservices | where {$_.Status -eq 'running'} #Laat alle services zien waar de status 'running' is
