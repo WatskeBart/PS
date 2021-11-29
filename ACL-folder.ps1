061284feec786b07b1fd3923c0f596ae46c80f9f
WatskeBart <bartistic@gmail.com>
2021-11-29 14:26:32 +0100

    Beschrijving toegevoegd

--- ACL-folder.ps1
+++ ACL-folder.ps1
@@ -1,5 +1,5 @@
-New-Item -Type Directory -Path "C:\testfolder"
-$acl = Get-Acl "c:\testfolder"
-$ar = New-Object System.Security.AccessControl.FileSystemAccessRule("accountnaam", "FullControl", "Allow")
-$acl.SetAccessRule($ar)
-Set-Acl -Path "c:\testfolder" -AclObject $acl
+New-Item -Type Directory -Path "C:\testfolder" #Maak map aan
+$acl = Get-Acl "c:\testfolder" #Haal huidige ACL op van de map en zet in variabele
+$ar = New-Object System.Security.AccessControl.FileSystemAccessRule("accountnaam", "FullControl", "Allow") #Variable met de te zetten rechten
+$acl.SetAccessRule($ar) #Zet rechten op de variabele
+Set-Acl -Path "c:\testfolder" -AclObject $acl #Plaats ACL op de map
