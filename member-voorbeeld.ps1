6971e5cccc70a9a9492ff5f51c4b6cbef919a3c0
WatskeBart <bartistic@gmail.com>
2021-11-29 14:53:23 +0100

    Group-Object toegevoegd

--- member-voorbeeld.ps1
+++ member-voorbeeld.ps1
@@ -1,6 +1,13 @@
 $mijnservices = Get-Service #Stop alle services in een variabele
+
 $mijnservices[0] #Laat de eerste services in de variabele lijst zien
+
 $mijnservices[0] | get-member #Laat alle beschikbare members zien van de eerste service
+
 $mijnservices[0].DisplayName #Laat de DisplayName zien van de eerste service, waarbij DisplayName een Property (member) is van de service
 
 $mijnservices | where {$_.Status -eq 'running'} #Laat alle services zien waar de status 'running' is
+
+$mijnservices | Sort-Object -Property Status -Descending #Sorteer alle services op de Property status aflopend
+
+$mijnservices | Group-Object -Property Status #Groepeer alle services op Status
