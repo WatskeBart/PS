cebfe43ee104ccb276affde54fbf054173db1721
WatskeBart <bartistic@gmail.com>
2021-11-29 14:59:00 +0100

    voorbeeld 2

--- member-voorbeeld.ps1
+++ member-voorbeeld.ps1
@@ -11,3 +11,7 @@ $mijnservices | where {$_.Status -eq 'running'} #Laat alle services zien waar de
 $mijnservices | Sort-Object -Property Status -Descending #Sorteer alle services op de Property status aflopend
 
 $mijnservices | Group-Object -Property Status #Groepeer alle services op Status
+
+$mijnservices | Select-Object -First 5 #Laat de eerste 5 services zien
+
+$mijnservices | Select-Object -Property Status, DisplayName #Laat alleen de Status en DisplayName zien van de services
