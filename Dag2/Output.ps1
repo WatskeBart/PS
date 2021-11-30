369cbf52fb617b54558a2c831c77a400fead78d9
WatskeBart <bartistic@gmail.com>
2021-11-30 11:33:08 +0100

    update

--- Dag2/Output.ps1
+++ Dag2/Output.ps1
@@ -1,15 +1,15 @@
-Get-Service | Out-File ./alleservices.txt #Schrijf alle informatie van Get-Service naar een tekstbestand genaamd 'alleservices.txt'
+Get-Service | Out-File alleservices-TXT.txt #Schrijf alle informatie van Get-Service naar een tekstbestand genaamd 'alleservices.txt'
 
-Get-Service > ./alleservices2.txt #Verkorte variant van bovenstaande
+Get-Service > alleservices-TXT2.txt #Verkorte variant van bovenstaande
 
-Get-Service | Export-Csv ./alleservices3.csv #Schrijf alle informatie van Get-Service naar een CSV (Comma Seperated Value) bestand
+Get-Service | Export-Csv alleservices-CSV.csv #Schrijf alle informatie van Get-Service naar een CSV (Comma Seperated Value) bestand
 
-Get-Service | Export-Csv ./alleservices3.csv -NoTypeInformation #Zelfde als bovenstaand, maar zonder de eerste regel (onnodige informatie)
+Get-Service | Export-Csv alleservices-CSV2.csv -NoTypeInformation #Zelfde als bovenstaand, maar zonder de eerste regel (onnodige informatie)
 
-$import = Import-Csv ./alleservices.txt #Zet alle info uit het CSV bestand in een variabele $import
+$import = Import-Csv alleservices-TXT3.txt #Zet alle info uit het CSV bestand in een variabele $import
 
-Get-Service | Export-Clixml ./alleservices4.xml #Schrijf naar XML
+Get-Service | Export-Clixml alleservices-XML.xml #Schrijf naar XML
 
-Get-Service | ConvertTo-Json | Out-File ./alleservices5.json #Converteer en schrijf naar JSON
+Get-Service | ConvertTo-Json | Out-File alleservices-JSON.json #Converteer en schrijf naar JSON
 
-Get-Service | ConvertTo-Html | Out-File ./alleservices6.html #Converteer en schrijf naar HTML
+Get-Service | ConvertTo-Html | Out-File alleservices-HTML.html #Converteer en schrijf naar HTML
