d9d1d05758cc5138cabed7fde82b356aac429680
WatskeBart <bartistic@gmail.com>
2021-11-30 13:56:46 +0100

    update

--- Dag2/PSProvider_PSDrive.ps1
+++ Dag2/PSProvider_PSDrive.ps1
@@ -10,8 +10,18 @@ Get-ChildItem WinDir: #Ga naar de PSProvider WinDir (C:\Windows)
 #of
 cd WinDir:
 
+## Bewerken van het register via PSDrive
 Set-Location HKCU: #Ga naar de registry HKEY Current User
 
 New-Item -Name 'TestKey' #Maak een nieuwe map in het register met de naam TestKey
 
 New-ItemProperty -Path HKCU:\TestKey\ -Name 'TestKey' -Value 'TESTWAARDE' #Geef de sleutel TestKey de waarde 'TESTWAARDE'
+
+## Bewerken van gebruikers in Active Directory via een nieuwe PSDrive 
+New-PSDrive -Name AdatumUsers -Root "CN=Users,DC=AdatumDC=com" -PSProvider ActiveDirectory #Maak een nieuwe PSDrive aan met de naam 'AdatumUsers' gekoppeld aan de OU users
+
+Set-Location AdatumUsers: #Ga naar de nieuwe PSDrive
+
+Get-ChildItem #Overzicht van aanwezige gebruikers en groepen
+
+New-Item -ItemType user -Path . -Name 'TestGebruiker' #Maak een gebruiker aan met de naam 'TestGebruiker'
