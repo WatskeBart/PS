ba7302411a9176be7cba4c9abe14ce67a885b496
WatskeBart <bartistic@gmail.com>
2021-12-01 08:46:46 +0100

    update

--- Dag1/ACL-folder.ps1
+++ Dag1/ACL-folder.ps1
@@ -3,3 +3,5 @@ $acl = Get-Acl "c:\testfolder" #Haal huidige ACL op van de map en zet in variabe
 $ar = New-Object System.Security.AccessControl.FileSystemAccessRule("accountnaam", "FullControl", "Allow") #Variable met de te zetten rechten
 $acl.SetAccessRule($ar) #Zet rechten op de variabele
 Set-Acl -Path "c:\testfolder" -AclObject $acl #Plaats ACL op de map
+
+Get-Acl C:\Temp | Format-List #Overzicht van NTFS rechten op de map C:\Temp
