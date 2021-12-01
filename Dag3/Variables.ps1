f57f15a31da6b5cfe637d8916fc3296f31da230a
WatskeBart <bartistic@gmail.com>
2021-12-01 09:10:40 +0100

    Variabelen

---
+++ Dag3/Variables.ps1
@@ -0,0 +1,6 @@
+$var1 = [string]$null #Geef de variabele $var1 een lege waarde
+$var2 = "iets" #Geef de variabele $var2 de waarde 'iets'
+
+if ($var1) { write-host "Var1 heeft een waarde" } else { write-host "Var1 heeft geen waarde" } #Geef een melding of de variabele $var1 een waarde heeft
+if ($var2) { write-host "Var2 heeft een waarde" } else { write-host "Var1 heeft geen waarde" } #Geef een melding of de variabele $var2 een waarde heeft
+
