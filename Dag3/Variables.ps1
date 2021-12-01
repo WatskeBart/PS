e5fa9a616888237f87cd386cc4e8c3908936b1db
WatskeBart <bartistic@gmail.com>
2021-12-01 10:54:45 +0100

    update

--- Dag3/Variables.ps1
+++ Dag3/Variables.ps1
@@ -4,3 +4,13 @@ $var2 = "iets" #Geef de variabele $var2 de waarde 'iets'
 if ($var1) { write-host "Var1 heeft een waarde" } else { write-host "Var1 heeft geen waarde" } #Geef een melding of de variabele $var1 een waarde heeft
 if ($var2) { write-host "Var2 heeft een waarde" } else { write-host "Var1 heeft geen waarde" } #Geef een melding of de variabele $var2 een waarde heeft
 
+#Dubbele quotes "" variabele gebruiken in een stuk tekst
+$var3 = "deze tekst"
+Write-Host "De variabele var3 heeft de waarde: $var3"
+#Enkele quotes '' variabele als tekst weergeven
+Write-Host 'De variabele var3 wordt nu niet vertaald: $var3'
+
+$var3.GetType() #Laat het type variabele zien, bijvoorbeeld String, Int32, Double, Bool etc.
+
+[double]$var4 = 10.5 #Voorbeeld van het type Double (getal met decimalen)
+$var4 #Laat waarde zien van variabele $var4
