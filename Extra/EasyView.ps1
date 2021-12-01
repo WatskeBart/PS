086879fd9b0c37389dc0366105f5dab3caac4c0d
WatskeBart <bartistic@gmail.com>
2021-12-01 09:02:35 +0100

    update

--- Extra/EasyView.ps1
+++ Extra/EasyView.ps1
@@ -1,3 +1,5 @@
 function EasyView { process { $_; Start-Sleep -seconds 1.5}} #Aanmaken van een functie waardoor iedere regel met een vertraging van 1.5 seconde wordt weergegeven
 
 Get-ChildItem C:\temp | EasyView #Laat de inhoud van C:\Temp zien en pipe deze naar de functie EasyView
+
+#Gebruik CTRL+C om het commando te stoppen indien dit te lang duurt
