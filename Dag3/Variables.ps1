89af93dfca72def5088759a5e23f9ee3489875db
WatskeBart <bartistic@gmail.com>
2021-12-01 11:06:07 +0100

    update

--- Dag3/Variables.ps1
+++ Dag3/Variables.ps1
@@ -1,6 +1,7 @@
 $var1 = [string]$null #Geef de variabele $var1 een lege waarde
 $var2 = "iets" #Geef de variabele $var2 de waarde 'iets'
 
+#Voorbeeld van If Else statement om te kijken of de variabele een waarde bevat.
 if ($var1) { write-host "Var1 heeft een waarde" } else { write-host "Var1 heeft geen waarde" } #Geef een melding of de variabele $var1 een waarde heeft
 if ($var2) { write-host "Var2 heeft een waarde" } else { write-host "Var1 heeft geen waarde" } #Geef een melding of de variabele $var2 een waarde heeft
 
