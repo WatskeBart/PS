9eb661b56423d9f2f215cd870f0ec93b638895b4
WatskeBart <bartistic@gmail.com>
2021-12-01 14:30:39 +0100

    update

--- Dag3/Scripting.ps1
+++ Dag3/Scripting.ps1
@@ -1,6 +1,11 @@
 Unblock-File #Verwijderd de downloaded status van een gedownload script
 
-
+$mijnservice = Get-Service -Name W32Time #Plaats de service W32Time in de variabele $mijnservice
+If ($mijnservice.Status -eq 'Running') {
+    Write-Host "Mijn service is gestart"
+} else {
+    Write-Host "Mijn service draait niet"
+} #Als de service gestart is geef dan een melding dat deze gestart is en anders een melding dat deze niet gestart is
 
 #Switch contstruct (keuze menu)
 $choice = Read-Host -Prompt "Kies 1, 2 of 3: "
