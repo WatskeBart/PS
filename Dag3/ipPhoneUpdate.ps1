c7821a8c2d68c278c6e45eb77b2910e6e59ca252
WatskeBart <bartistic@gmail.com>
2021-12-02 09:35:04 +0100

    update

--- Dag3/ipPhoneUpdate.ps1
+++ Dag3/ipPhoneUpdate.ps1
@@ -1,1 +1,7 @@
-#ipPhone attribute in AD 
+$users = Get-ADGroupMember IPPhoneTest
+
+ForEach ($user in $users) {
+    $fullUser = Get-ADUser $user
+    $ipPhone = $fullUser.GivenName + "." + $fullUser.Surname + "@adatum.com"
+    Set-ADUser $fullUser -Replace @{ipPhone="$ipPhone"}
+}
