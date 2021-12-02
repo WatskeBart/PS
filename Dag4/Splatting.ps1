6dc11d9e44699ad881abccf95fbf58f4d6c3deae
WatskeBart <bartistic@gmail.com>
2021-12-02 11:42:35 +0100

    Splatting

---
+++ Dag4/Splatting.ps1
@@ -0,0 +1,5 @@
+$opties = @{
+    ComputerName = "localhost" <#Parameter met de waarde#>
+}
+
+Test-Connection @opties #Start commando met de parameters en waarden zoals aangegeven in de hashtable $opties
