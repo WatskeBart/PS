322a9667bba0402b53b44c766aacd2ec547a5d27
WatskeBart <bartistic@gmail.com>
2021-12-02 19:38:59 +0100

    update

--- Extra/PowerShell GUI/MessageBox.ps1
+++ Extra/PowerShell GUI/MessageBox.ps1
@@ -1,23 +1,25 @@
-$msgBoxInput =  [System.Windows.MessageBox]::Show('Would you like to play a game?','Game  input','YesNoCancel','Error')
+Add-Type -AssemblyName PresentationCore, PresentationFramework #Toevoegen van de juiste Assemblies
+$msgBoxInput = [System.Windows.MessageBox]::Show('Maak een keuze', 'Keuze venster', 'YesNoCancel', 'Question') #Opmaken van het venster
 
-  switch  ($msgBoxInput) {
+#Switch statement om bij een bepaalde keuze een opdracht uit te voeren.
+switch ($msgBoxInput) {
 
   'Yes' {
 
-  ## Do something 
+    Write-Host "Je hebt ja gekozen"
 
   }
 
   'No' {
 
-  ## Do something
+    Write-Host "Je hebt nee gekozen"
 
   }
 
   'Cancel' {
 
-  ## Do something
+    Write-Host "Je hebt annuleren gekozen"
 
   }
 
-  }
+}
