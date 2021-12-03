7f5129b3e153b3841fefa9cdccabd5f689fa81f1
WatskeBart <bartistic@gmail.com>
2021-12-03 09:16:57 +0100

    update

--- Extra/Weerbericht.ps1
+++ Extra/Weerbericht.ps1
@@ -1,2 +1,4 @@
 Clear-Host #Maak het scherm leeg
-Invoke-RestMethod http://wttr.in/"Eindhoven,Netherlands" #Vraag de weersverwachting op via Invoke-RestMethod
+Invoke-RestMethod wttr.in/Eindhoven #Vraag de weersverwachting op via Invoke-RestMethod
+Invoke-RestMethod v2d.wttr.in/Eindhoven #Grafiek met temperatuur en regen verwachting
+Invoke-RestMethod v3.wttr.in/Eindhoven #Geografische kaart met actueel weer (werkt momenteel alleen in een browser)
