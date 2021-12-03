188da0a299d2f6dec2467af79381f2f0f96eff4e
WatskeBart <bartistic@gmail.com>
2021-12-03 11:33:01 +0100

    Versie 2

--- Dag5/VrijeRuimte.ps1
+++ Dag5/VrijeRuimte.ps1
@@ -5,10 +5,10 @@
 #Geef een output in het geel bij vrije ruimte minder dan 10GB
 #Zorg dat in de output de driveletters in oplopende volgorde worden weergegeven
 
-$disks = Get-Volume | Where-Object { $_.FileSystemType -eq "NTFS" } | Sort-Object -Property DriveLetter
+$disks = Get-Volume | Where-Object { $_.FileSystemType -eq "NTFS" -and $_.DriveLetter -ne "" } | Sort-Object -Property DriveLetter
 
 foreach ($disk in $disks) {
     If ($disk.SizeRemaining -ge 20GB) { Write-Host -BackgroundColor Blue "Genoeg ruimte op: $($disk.DriveLetter), namelijk: $($disk.SizeRemaining)" }
-    elseif ($disk.SizeRemaining -le 20GB) { Write-Host -BackgroundColor Yellow "Niet genoeg ruimte op: $($disk.DriveLetter), namelijk: $($disk.SizeRemaining)" }
     elseif ($disk.SizeRemaining -le 10GB) { Write-Host -BackgroundColor Red "Schijfruimte kritiek op: $($disk.DriveLetter), namelijk: $($disk.SizeRemaining)" }
+    elseif ($disk.SizeRemaining -le 20GB) { Write-Host -BackgroundColor Yellow "Niet genoeg ruimte op: $($disk.DriveLetter), namelijk: $($disk.SizeRemaining)" }
 }
