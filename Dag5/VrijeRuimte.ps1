807f7d4f7c44abb3ca5420c539f0b54563055313
WatskeBart <bartistic@gmail.com>
2021-12-03 11:58:28 +0100

    update

--- Dag5/VrijeRuimte.ps1
+++ Dag5/VrijeRuimte.ps1
@@ -9,12 +9,12 @@ $disks = Get-Volume | Where-Object { $_.FileSystemType -eq "NTFS" -and $_.DriveL
 
 foreach ($disk in $disks) {
     if ($disk.SizeRemaining -le 10GB) {
-        Write-Host -BackgroundColor Red "Schijfruimte kritiek op: $($disk.DriveLetter), namelijk: $([math]::Round($disk.SizeRemaining / 1GB))GB"
+        Write-Host -BackgroundColor Red "Schijfruimte kritiek op: $($disk.DriveLetter) Vrije ruimte: $([math]::Round($disk.SizeRemaining / 1GB))GB"
     }
     elseif ($disk.SizeRemaining -le 20GB) {
-        Write-Host -BackgroundColor Yellow "Niet genoeg ruimte op: $($disk.DriveLetter), namelijk: $([math]::Round($disk.SizeRemaining / 1GB))GB"
+        Write-Host -BackgroundColor Yellow "Niet genoeg ruimte op: $($disk.DriveLetter) Vrije ruimte: $([math]::Round($disk.SizeRemaining / 1GB))GB"
     }
     elseif ($disk.SizeRemaining -ge 20GB) {
-        Write-Host -BackgroundColor Blue "Genoeg ruimte op: $($disk.DriveLetter), namelijk: $([math]::Round($disk.SizeRemaining / 1GB))GB"
+        Write-Host -BackgroundColor Blue "Genoeg ruimte op: $($disk.DriveLetter) Vrije ruimte: $([math]::Round($disk.SizeRemaining / 1GB))GB"
     }
 }
