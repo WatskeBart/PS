3d214b57a1f5c8a00321fdbf98be536ab9a08280
WatskeBart <bartistic@gmail.com>
2021-12-03 11:24:56 +0100

    Opdracht Dag 5

---
+++ Dag5/VrijeRuimte.ps1
@@ -0,0 +1,14 @@
+###Opdracht Dag 5
+#Check alle NTFS disk op vrije ruimte
+#Geef een output in het blauw bij genoeg vrije ruimte
+#Geef een output in het geel bij vrije ruimte minder dan 20GB
+#Geef een output in het geel bij vrije ruimte minder dan 10GB
+#Zorg dat in de output de driveletters in oplopende volgorde worden weergegeven
+
+$disks = Get-Volume | Where-Object { $_.FileSystemType -eq "NTFS" } | Sort-Object -Property DriveLetter
+
+foreach ($disk in $disks) {
+    If ($disk.SizeRemaining -ge 20GB) { Write-Host -BackgroundColor Blue "Genoeg ruimte op: $($disk.DriveLetter), namelijk: $($disk.SizeRemaining)" }
+    elseif ($disk.SizeRemaining -le 20GB) { Write-Host -BackgroundColor Yellow "Niet genoeg ruimte op: $($disk.DriveLetter), namelijk: $($disk.SizeRemaining)" }
+    elseif ($disk.SizeRemaining -le 10GB) { Write-Host -BackgroundColor Red "Schijfruimte kritiek op: $($disk.DriveLetter), namelijk: $($disk.SizeRemaining)" }
+}
